9b5a1712222a0ba7551feb24439e701413681a09
remi <remi@remitaylor.com>
2011-01-27 03:14:47 -0700

    Organizing lib some more.

--- lib/odata-provider.rb
+++ lib/odata-provider.rb
@@ -2,20 +2,19 @@ $LOAD_PATH.unshift File.dirname(__FILE__)
 
 %w[ rubygems active_support/inflector rack nokogiri ].each {|lib| require lib }
 
-require 'odata-provider/extensions'
-
 # OData namespace
 module OData
 end
 
+require 'odata-provider/extensions'
 require 'odata-provider/initialized_by_attributes'
 require 'odata-provider/query_executor'
-require 'odata-provider/ruby_query_executor'
+require 'odata-provider/query_executors/ruby_query_executor'
 require 'odata-provider/query'
 require 'odata-provider/query_option'
-require 'odata-provider/key_query_option'
-require 'odata-provider/top_query_option'
-require 'odata-provider/skip_query_option'
+require 'odata-provider/query_options/key_query_option'
+require 'odata-provider/query_options/top_query_option'
+require 'odata-provider/query_options/skip_query_option'
 require 'odata-provider/provider'
 require 'odata-provider/provider/rack/middleware'
 require 'odata-provider/provider/rack/application'
